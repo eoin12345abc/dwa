b862cb63ed6a92e5304eb9be02a5d9fb8b67e463
Eoin <eoin12345abc@gmail.com>
2012-11-06 05:14:17 +0900

    Added Last Name to Profile

--- p2.eoinmoore.net/views/v_users_profile.php
+++ p2.eoinmoore.net/views/v_users_profile.php
@@ -1,4 +1,4 @@
-<h1>This is the profile of <?=$user->first_name?></h1>
+<h1>This is the profile of <?=$user->first_name?>  <?=$user->last_name?></h1>
 <? foreach($own_posts as $own_post): ?>
 <?$date = date("D, d M Y", $own_post['created']);?>
 <?$time = date("H:i:s", $own_post['created']);?>
